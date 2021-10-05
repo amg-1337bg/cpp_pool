b46dba8e6b967cf4fc6daafc50544dcb205723d7
Brahim Amghough <bamghoug@e0r2p7.1337.ma>
2021-10-05 12:02:43 +0100

    first push

---
+++ DAY00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctype.h>
+
+int main(int ac, char **av)
+{
+	int i;
+	int c;
+
+	i = 0;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	while (av[++i] != NULL)
+	{
+		c = -1;
+		while(av[i][++c] != 0)
+			std::cout << (char)std::toupper(av[i][c]);
+		if (av[i + 1] != NULL)
+			std::cout << " ";
+		// std::cout << av[i] << std::endl;
+	}
+	std::cout << std::endl;
+	return (0);
+}
