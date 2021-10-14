13a85a204e89f75f86467784258964e4ba1b90fd
amg-1337bg <ibros1@Asus-ZenBook.localdomain>
2021-10-14 17:09:56 +0100

    ex04 done

--- DAY01/ex04/main.cpp
+++ DAY01/ex04/main.cpp
@@ -3,16 +3,42 @@
 
 int main(int ac, char **av)
 {
-	std::string read;
+	std::string s1;
+	std::string s2;
+	std::string filename;
+	std::string filenamereplace;
+	std::string	read;
+	std::string	write;
+	int			occur;
 
+	s1 = av[2];
+	s2 = av[3];
+	filename = av[1];
 	if (ac != 4)
 	{
-		std::cout << "Error" << std::endl;
+		std::cout << "Error!" << std::endl;
 		return (1);
 	}
-	std::fstream file;
-	file.open(av[1], std::ios::in);
-	std::getline(file, read);
-	std::cout << read << std::endl;
+	if(s1.empty() || s2.empty() || filename.empty())
+	{
+		std::cout << "one of the argument is empty!" << std::endl;
+		return (1);
+	}
+	filenamereplace = filename + ".replace";
+	std::cout << filenamereplace << std::endl;
+	std::ifstream ifstr(filename);
+	std::ofstream ofstream(filenamereplace);
+	while (std::getline(ifstr, read))
+	{
+		while ((occur = read.find(s1)) != std::string::npos)
+		{
+			write.append(read, 0, occur);
+			write.append(s2);
+			read.erase(0, occur + s1.length());
+		}
+		write.append(read, 0, read.length());
+		ofstream << write << std::endl;
+		write.clear();
+	}
 	return (0);
 }
