9f41770f5b0f70ee53ece2f3115c907093f854ad
Brahim Amghough <bamghoug@e0r3p10.1337.ma>
2021-10-14 20:10:09 +0100

    test

--- DAY01/ex04/main.cpp
+++ DAY01/ex04/main.cpp
@@ -4,15 +4,35 @@
 int main(int ac, char **av)
 {
 	std::string read;
+	std::size_t ret;
+	int begin;
+	std::string toWrite;
+	char *s = NULL;
+	std::string s1;
+	std::string s2;
 
 	if (ac != 4)
 	{
 		std::cout << "Error" << std::endl;
 		return (1);
 	}
+	s1 = av[2];
+	s2 = av[3];
 	std::fstream file;
-	file.open(av[1], std::ios::in);
-	std::getline(file, read);
-	std::cout << read << std::endl;
+	file.open(av[1]);
+	if (file.fail())
+	{
+		std::cout << "failed to open the file" << std::endl;
+		return (1);
+	}
+	while (std::getline(file, read))
+	{
+		begin = 0;
+		while ((ret = read.find(s1, begin)) != std::string::npos)
+		{
+			
+		}
+	}
+	// std::cout << read << std::endl;
 	return (0);
 }
