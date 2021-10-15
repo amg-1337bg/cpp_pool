fd350b1e3885cbfd4cb6f59f3f60f3e4b664534d
Brahim Amghough <bamghoug@e0r3p10.1337.ma>
2021-10-15 12:09:33 +0100

    to pull

--- DAY01/ex04/main.cpp
+++ DAY01/ex04/main.cpp
@@ -3,15 +3,6 @@
 
 int main(int ac, char **av)
 {
-<<<<<<< HEAD
-	std::string read;
-	std::size_t ret;
-	int begin;
-	std::string toWrite;
-	char *s = NULL;
-	std::string s1;
-	std::string s2;
-=======
 	std::string s1;
 	std::string s2;
 	std::string filename;
@@ -19,44 +10,27 @@ int main(int ac, char **av)
 	std::string	read;
 	std::string	write;
 	int			occur;
->>>>>>> 13a85a204e89f75f86467784258964e4ba1b90fd
 
-	s1 = av[2];
-	s2 = av[3];
-	filename = av[1];
 	if (ac != 4)
 	{
 		std::cout << "Error!" << std::endl;
 		return (1);
 	}
-<<<<<<< HEAD
 	s1 = av[2];
 	s2 = av[3];
-	std::fstream file;
-	file.open(av[1]);
-	if (file.fail())
-	{
-		std::cout << "failed to open the file" << std::endl;
-		return (1);
-	}
-	while (std::getline(file, read))
-	{
-		begin = 0;
-		while ((ret = read.find(s1, begin)) != std::string::npos)
-		{
-			
-		}
-	}
-	// std::cout << read << std::endl;
-=======
+	filename = av[1];
 	if(s1.empty() || s2.empty() || filename.empty())
 	{
 		std::cout << "one of the argument is empty!" << std::endl;
 		return (1);
 	}
 	filenamereplace = filename + ".replace";
-	std::cout << filenamereplace << std::endl;
 	std::ifstream ifstr(filename);
+	if (ifstr.fail())
+	{
+		std::cout << "Error While opening the file" << std::endl;
+		return (1);
+	}
 	std::ofstream ofstream(filenamereplace);
 	while (std::getline(ifstr, read))
 	{
@@ -70,6 +44,5 @@ int main(int ac, char **av)
 		ofstream << write << std::endl;
 		write.clear();
 	}
->>>>>>> 13a85a204e89f75f86467784258964e4ba1b90fd
 	return (0);
 }
