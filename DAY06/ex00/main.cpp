a008043c695135b435ded61cb2071131464cb382
Brahim Amghough <bamghoug@e1r12p8.1337.ma>
2021-11-24 20:11:49 +0100

    finished ex00 in day06

--- DAY06/ex00/main.cpp
+++ DAY06/ex00/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <sstream>
 #include <stdlib.h>
+#include <cmath>
 
-void	toChar(char *s)
+void	toChar(std::string str)
 {
-	int num = atoi(s);
+	int num;
+	std::stringstream stringstream(str);
+	stringstream >> num;
+	if(stringstream.fail())
+	{
+		std::cout << "char : Impossible" << std::endl;
+		return ;
+	}
 	if (num >= 0 && num <= 255)
 	{
 		char c = static_cast<char>(num);
@@ -13,13 +22,20 @@ void	toChar(char *s)
 			std::cout << "char : Non displayable" << std::endl;
 		return ;
 	}
-	std::cout << "char : Non displayable" << std::endl;
+	std::cout << "char : Impossible" << std::endl;
 }
 
-void	toInt(char *s)
+void	toInt(std::string str)
 {
-	long num = atol(s);
+	int num;
 
+	std::stringstream stringstream(str);
+	stringstream >> num;
+	if(stringstream.fail())
+	{
+		std::cout << "int : Impossible" << std::endl;
+		return ;
+	}
 	if (num < std::numeric_limits<int>::min() || num > std::numeric_limits<int>::max())
 	{
 		std::cout << "int : Impossible" << std::endl;
@@ -28,25 +44,29 @@ void	toInt(char *s)
 	std::cout << "int : " << num << std::endl;
 }
 
-void	toFloat(char *s)
+void	toFloat(std::string str)
 {
-	float num = atof(s);
+	float num;
 	int tocon;
 
+	std::stringstream stringstream(str);
+	stringstream >> num;
 	tocon = static_cast<int> (num);
-	if (num > tocon)
+	if (num != tocon)
 		std::cout << "float : " << num << "f" << std::endl;
 	else
 		std::cout << "float : " << num << ".0f" << std::endl;
 }
 
-void	toDouble(char *s)
+void	toDouble(std::string str)
 {
-	double num = strtod(s, NULL);
+	double num;
 	int tocon;
 
+	std::stringstream stringstream(str);
+	stringstream >> num;
 	tocon = static_cast<int> (num);
-	if (num > tocon)
+	if (num != tocon)
 		std::cout << "double : " << num << std::endl;
 	else
 		std::cout << "double : " << num << ".0" << std::endl;
@@ -56,11 +76,11 @@ int main(int ac, char *av[])
 {
 	if (ac == 2)
 	{
-		// std::cout << strtod(av[1], NULL) << std::endl;
-		toChar(av[1]);
-		toInt(av[1]);
-		toFloat(av[1]);
-		toDouble(av[1]);
+		std::string s = av[1];
+		toChar(s);
+		toInt(s);
+		toFloat(s);
+		toDouble(s);
 		return (0);
 	}
 	std::cout << "Program Needs One Parameter!\n";
