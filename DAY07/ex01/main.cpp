7467a52d606d13c5c2ba2773ec9b0ef484f40579
Brahim Amghough <bamghoug@e0r4p7.1337.ma>
2021-11-26 23:13:58 +0100

    sstill

--- DAY07/ex01/main.cpp
+++ DAY07/ex01/main.cpp
@@ -1,14 +1,32 @@
 #include "iter.hpp"
 
-void	toupp(char &str)
+void	toH(char &str)
 {
-	str = 'h';
+	str = 'H';
+}
+
+void	printString(std::string &str)
+{
+	std::cout << str << std::endl;
+}
+
+void	tohundred(int &i)
+{
+	i = 100;
 }
 
 int main()
 {
 	char str[] = "hello";
+	std::string s[] = {"hello", "world", "how are you"};
+	int	i[] = {187, 12, 13};
 
-	iter<char>(str, 5, swap);
+	iter<char>(str, 5, toH);
 	std::cout << str << std::endl;
+
+	iter<std::string>(s, 3, printString);
+
+	iter<int>(i, 3, tohundred);
+	for (size_t j = 0; j < 3; j++)
+		std::cout << i[j] << std::endl;
 }
